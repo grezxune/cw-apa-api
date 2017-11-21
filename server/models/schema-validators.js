f918e01c7c5674f92aa28b03941a8b13177a292f
tommy <tomtrezb2003@gmail.com>
2017-11-21 11:40:52 -0600

    Added log for phone value for debugging

--- server/models/schema-validators.js
+++ server/models/schema-validators.js
@@ -2,14 +2,15 @@ const validator = require('validator');
 
 const phoneValidator = {
     validator: (value) => {
-        return validator.isMobilePhone(value, 'en-US');
+        console.log('*** PHONE ***: ', value);
+        return value && validator.isMobilePhone(value, 'en-US');
     },
     message: '{VALUE} is not a valid phone number'
 };
 
 const emailValidator = {
     validator: (value) => {
-        return validator.isEmail(value);
+        return value && validator.isEmail(value);
     },
     message: '{VALUE} is not a valid email'
 };
