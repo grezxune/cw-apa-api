f513abdb91ab2685c7e45213b13e28ba9f99c2a7
tommy <tomtrezb2003@gmail.com>
2017-11-21 11:48:11 -0600

    Fixed phone validation

--- server/models/schema-validators.js
+++ server/models/schema-validators.js
@@ -2,15 +2,14 @@ const validator = require('validator');
 
 const phoneValidator = {
     validator: (value) => {
-        console.log('*** PHONE ***: ', value);
-        return value && validator.isMobilePhone(value, 'en-US');
+        return !value || validator.isMobilePhone(value, 'en-US');
     },
     message: '{VALUE} is not a valid phone number'
 };
 
 const emailValidator = {
     validator: (value) => {
-        return value && validator.isEmail(value);
+        return validator.isEmail(value);
     },
     message: '{VALUE} is not a valid email'
 };
