d2a2d2d9833750b08e5214d5f8f1a10e2d9b91c8
grezxune <tomtrezb2003@gmail.com>
2018-08-30 16:54:28 -0500

    Testing calendar stuff

--- server/config/config.js
+++ server/config/config.js
@@ -3,19 +3,26 @@ const fs = require('fs');
 const env = process.env.NODE_ENV || 'development';
 
 const readConfigFile = () => {
-    fs.readFile(path.join(__dirname, `.config.${env}.json`), 'utf-8', (err, data) => {
-        if (!err) {
-            const fileConfigs = JSON.parse(data);
+    fs.readFile(
+        path.join(__dirname, `.config.${env}.json`),
+        'utf-8',
+        (err, data) => {
+            if (!err) {
+                const fileConfigs = JSON.parse(data);
 
-            process.env.TOKEN_SECRET = fileConfigs.TOKEN_SECRET;
-        } else {
-            console.log(`Error parsing settings file for ${env} environment`, err);
+                process.env.TOKEN_SECRET = fileConfigs.TOKEN_SECRET;
+            } else {
+                console.log(
+                    `Error parsing settings file for ${env} environment`,
+                    err
+                );
+            }
         }
-    });
+    );
 };
 
 if (env === 'development') {
-    process.env.PORT = 3000;
+    process.env.PORT = 3009;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/cw-apa';
     process.env.ALLOWED_ORIGIN = 'http://localhost:8080';
     readConfigFile();
--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 require('./config/config');
 
-
 /// *** External Packages *** ///
 const { ObjectID } = require('mongodb');
 const fs = require('fs');
@@ -8,14 +7,17 @@ const _ = require('lodash');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const moment = require('moment');
-
+const ics = require('ics');
+const ical = require('ical-generator');
+const cal = ical({ domain: 'cw-apa-api' })
+    .url('http://cw-apa-api.herokuapp.com/calendar.ics')
+    .ttl(10);
 
 /// *** Custom Variables *** ///
 const port = process.env.PORT;
 const { mongoose } = require('./db/mongoose');
 var { app } = require('./app');
 const { allowCrossOrigin } = require('./middleware/cross-origin');
-
 
 /// *** Middlewares *** ///
 app.use(allowCrossOrigin);
@@ -25,10 +27,12 @@ app.use(cookieParser());
 app.use((req, res, next) => {
     const now = new moment().format('MMM Do YYYY hh:mm');
     const authCookie = req.cookies['auth'];
-    const log = `${now}: ${req.method} - ${req.url}\n******** Auth Cookie ********\n${authCookie}`;
+    const log = `${now}: ${req.method} - ${
+        req.url
+    }\n******** Auth Cookie ********\n${authCookie}`;
 
     // console.log(log);
-    fs.appendFile('server.log', `${log}\n`, (err) => {
+    fs.appendFile('server.log', `${log}\n`, err => {
         if (err) {
             console.log('Unable to append to server.log.');
         }
@@ -37,12 +41,14 @@ app.use((req, res, next) => {
 });
 
 // Parse form data into body
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
 
 // Parse body and put it into req.body
-app.use(bodyParser.json({limit: '200mb'}));
+app.use(bodyParser.json({ limit: '200mb' }));
 
 // app.use((req, res, next) => {
 //     console.log(res.getHeaders());
@@ -51,9 +57,46 @@ app.use(bodyParser.json({limit: '200mb'}));
 
 // Home route
 app.get('/', (req, res) => {
-    res.send('<h1 style="text-align: center;">Hello, welcome to Central Wisconsin APA Admin!</h1>');
+    res.send(
+        '<h1 style="text-align: center;">Hello, welcome to Central Wisconsin APA Admin! <a href="/calendar.ics">CALENDAR!</a></h1>'
+    );
 });
 
+app.get('/calendar.ics', (req, res) => {
+    console.log('HIT CALENDAR ENDPOINT!');
+    const event = cal.createEvent({
+        summary: 'My Event - MUAHAHA',
+        start: moment(),
+        uid: 12345
+    });
+    const filename = `${__dirname}/calendarFiles/event.ics`;
+    cal.save(filename, (err, val) => res.sendFile(filename));
+    // const event = await ics.createEvent(
+    //     {
+    //         title: 'Dinner - EDITED!!!',
+    //         description: 'Nightly thing I do',
+    //         start: [2018, 1, 15, 6, 30],
+    //         duration: { minutes: 50 },
+    //         'X-PUBLISHED-TTL': 'PT1M'
+    //     },
+    //     (error, event) => {
+    //         console.log('Done creating event with ics');
+    //         console.log(event);
+    //         if (error) {
+    //             console.log(error);
+    //         }
+
+    //         if (event) {
+    //             console.log('EVENT!: \n', event);
+    //             const filename = `${__dirname}/calendarFiles/event.ics`;
+    //             fs.writeFileSync(filename, event);
+    //             res.sendFile(filename);
+    //         } else {
+    //             res.send('<h1>ERROR WITH CALENDAR</h1>');
+    //         }
+    //     }
+    // );
+});
 
 /// *** CUSTOM ROUTES *** ///
 require('./routes/news-item');
@@ -61,7 +104,6 @@ require('./routes/event-item');
 require('./routes/user');
 require('./routes/player');
 require('./routes/team');
-
 
 // Start Express server
 app.listen(port, () => {
